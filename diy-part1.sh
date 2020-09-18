2b166f48e2a4a9f16004a0c0a966749fe5e9d9bf
jcfads <47596409+jcfads@users.noreply.github.com>
2020-09-18 14:59:41 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,3 +15,5 @@
 
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+sed -i '$a src-git 1 https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git 2 https://github.com/kenzok8/openwrt-packages' feeds.conf.default
